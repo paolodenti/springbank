e9b83c7984cc01dd6e35c4ccf13e15760e7e5fa9
Paolo Denti <paolo.denti@gmail.com>
2021-10-03 20:45:52 -0700

    temporary security refactored

--- user.cmd.api/src/main/java/com/github/paolodenti/springbank/user/cmd/api/config/SecurityConfig.java
+++ user.cmd.api/src/main/java/com/github/paolodenti/springbank/user/cmd/api/config/SecurityConfig.java
@@ -6,12 +6,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
+// TODO access open, only for initial development
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.antMatchers("/**").permitAll().anyRequest().authenticated()).csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.antMatchers("/**").permitAll()).csrf(AbstractHttpConfigurer::disable);
     }
 }
