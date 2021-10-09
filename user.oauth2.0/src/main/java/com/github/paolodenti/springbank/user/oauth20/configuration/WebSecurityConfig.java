1919bec430a4b13267a11a39cbdd7bfc1d5ab8fe
Paolo Denti <paolo.denti@gmail.com>
2021-10-08 22:15:02 -0700

    oauth2 server

--- user.oauth2.0/src/main/java/com/github/paolodenti/springbank/user/oauth20/configuration/WebSecurityConfig.java
+++ user.oauth2.0/src/main/java/com/github/paolodenti/springbank/user/oauth20/configuration/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -38,11 +37,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring();
     }
 
     @Override
